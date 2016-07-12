06f32dab8e26f71e6929a3460250ff8beeac4ab6
Savitha Pinnepalli <savitha@Savithas-MacBook-Air.local>
2016-07-12 03:56:17 -0500

    Array add

---
+++ contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var mid = [50, 60, 70, 80]
+var final = [50, 40, 30, 20]
+
+var total = [0,0,0,0]
+
+for i in total
+{
+    total[i] = mid[i] + final[i]
+    print("total = \(total[i])")
+}
